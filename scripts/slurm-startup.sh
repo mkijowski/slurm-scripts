3adae5317a879758442e7da53aa56d53a047d650
Matthew Kijowski <matthewkijowski@gmail.com>
2019-05-08 10:51:43 -0400

    added debugging to slurm-startud

--- scripts/slurm-startup.sh
+++ scripts/slurm-startup.sh
@@ -6,10 +6,16 @@ exec &> >( logger -t slurm-startup )
 while true
 do
   if mountpoint -q /home; then
+    echo "/home mounted"
+    sleep 1
+    echo "Initializing nvidia devices"
     /etc/slurm/scripts/deviceQuery > /dev/null
+    echo "nvidia init result: $?"
+    sleep 5
     systemctl start slurmd
     break
   fi
+  echo "No /home yet"
   sleep 10
 done
 
