420b52d9f71f07f6b28e957b71feba18c29f4879
Matthew Kijowski <matthewkijowski@gmail.com>
2019-05-08 11:13:10 -0400

    some tweaks

--- scripts/slurm-startup.sh
+++ scripts/slurm-startup.sh
@@ -3,7 +3,7 @@
 #enable logging
 exec &> >( logger -t slurm-startup )
 
-while true
+for i in 1 2 3 4 5
 do
   if mountpoint -q /home; then
     echo "/home mounted"
@@ -11,11 +11,18 @@ do
     echo "Initializing nvidia devices"
     /etc/slurm/scripts/deviceQuery > /dev/null
     echo "nvidia init result: $?"
-    sleep 5
+    sleep 1
+    echo "Restarting slurmd service"
     systemctl start slurmd
+    echo "slurmd service restart result: $?"
+    echo "slurmd startup successful"
     break
   fi
-  echo "No /home yet"
+  echo "No /home yet, sleeping 10s"
   sleep 10
 done
+
+echo "error, /home failed to mount in 60s"
+echo "Did you forget to say the magic word?"
+
 
