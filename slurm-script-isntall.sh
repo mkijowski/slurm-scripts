a8cf1406514cacd6bcb9b88e4365d8a25a9027f9
Matthew Kijowski <matthewkijowski@gmail.com>
2019-05-08 09:35:38 -0400

    init install script

---
+++ slurm-script-isntall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Must be root / sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root, exiting."
+   exit 1
+fi
+
+# must have mkijowski
+if [ ! -d /home/mkijowski ]; then
+   read -p "Home directory for user mkijowski not found, enter username:" WHOAMI
+else
+   WHOAMI=mkijowski
+fi
+
+USERDIR=/home/$WHOAMI
+SLURMSCRIPTDIR=/etc/slurm/scripts
+GITDIR=$USERDIR/git/slurm-scripts
+
+if [ ! -d $GITDIR ]; then
+   echo "Cannot find git repo @ $GITDIR , exiting."
+   exit 1
+fi
+
+mkdir -p $SLURMSCRIPTDIR
+
+ln -sfb $GITDIR/scripts/epilog.sh $SLURMSCRIPTDIR/epilog.sh
+ln -sfb $GITDIR/scripts/taskprolog.sh $SLURMSCRIPTDIR/taskprolog.sh 
+
